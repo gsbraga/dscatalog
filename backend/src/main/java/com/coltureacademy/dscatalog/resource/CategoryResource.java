cec164e204b26f0ceb34027b64138235161afa38
Genilson Braga <genilsonsoares.b@gmail.com>
2021-11-13 13:17:57 -0300

    Insert DTO.

--- backend/src/main/java/com/coltureacademy/dscatalog/resource/CategoryResource.java
+++ backend/src/main/java/com/coltureacademy/dscatalog/resource/CategoryResource.java
@@ -1,6 +1,6 @@
 package com.coltureacademy.dscatalog.resource;
 
-import com.coltureacademy.dscatalog.entities.Category;
+import com.coltureacademy.dscatalog.dto.CategoryDTO;
 import com.coltureacademy.dscatalog.services.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -19,8 +18,8 @@ public class CategoryResource {
     private CategoryService service;
 
     @GetMapping
-    public ResponseEntity<List<Category>> findAll() {
-       List<Category> list = service.findAll();
+    public ResponseEntity<List<CategoryDTO>> findAll() {
+       List<CategoryDTO> list = service.findAll();
         return ResponseEntity.ok().body(list);
     }
 }
