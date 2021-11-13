a3e0c530bd2860e52899459caec9d82890de9b8f
Genilson Braga <genilsonsoares.b@gmail.com>
2021-11-13 12:58:08 -0300

    Database seed

--- backend/src/main/java/com/coltureacademy/dscatalog/entities/Category.java
+++ backend/src/main/java/com/coltureacademy/dscatalog/entities/Category.java
@@ -5,7 +5,7 @@ import java.io.Serializable;
 import java.util.Objects;
 
 @Entity
-@Table(name = "tbl_category")
+@Table(name = "tb_category")
 public class Category implements Serializable {
     private static final long serialVersionUID = 1L;
 
