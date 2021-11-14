6accf85c5d01314084935e0467ba5532ab6603c4
Genilson Braga <genilsonsoares.b@gmail.com>
2021-11-14 19:11:18 -0300

    POST category

--- backend/src/main/java/com/coltureacademy/dscatalog/resource/CategoryResource.java
+++ backend/src/main/java/com/coltureacademy/dscatalog/resource/CategoryResource.java
@@ -5,7 +5,9 @@ import com.coltureacademy.dscatalog.services.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -25,5 +27,13 @@ public class CategoryResource {
     public ResponseEntity<CategoryDTO> findById(@PathVariable Long id) {
         CategoryDTO dto = service.findById(id);
         return ResponseEntity.ok().body(dto);
+    }
+
+    @PostMapping
+    public ResponseEntity<CategoryDTO> insert (@RequestBody CategoryDTO dto){
+        dto = service.insert(dto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(dto.getId()).toUri();
+        return ResponseEntity.created(uri).body(dto);
     }
 }
